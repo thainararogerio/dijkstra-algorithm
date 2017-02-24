180f0cf33d6f5907dfffb3853b11d88949463a0a
Thainara Rogério <thainara.rogerio@senior.com.br>
2017-02-23 21:07:05 -0300

    First implementation, after months of non-practice, of the Dijkstra Algorithm as a part of my research on that algorithm.

---
+++ VerticeSimples.java
@@ -0,0 +1,12 @@
+package tcc.algoritmo.dijkstra;
+
+public class VerticeSimples {
+	public int distancia;
+	public boolean visitado;
+	
+	public VerticeSimples(int d, boolean v)
+	{
+		this.distancia = d;
+		this.visitado = v;
+	}
+}
